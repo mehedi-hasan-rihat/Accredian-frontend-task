efb8999884f0834d9074be4e0afa7a5a6ab4dcea
mehedi-15 <mehedi4794@gmail.com>
2025-02-13 09:11:10 +0600

    add announcements

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import Announcements from './components/Announcements'
+export default function App() {
   return (
-   <div>
-    
-   </div>
+    <div>
+      <Announcements/>
+    </div>
   )
 }
-
-export default App
---
+++ src/components/Announcements.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Announcements() {
+  return (
+    <div className="bg-[#1A73E826] py-3">
+      {" "}
+      <div className=" mx-auto w-max">
+        <p>
+          Navigate your ideal career path with tailored expert advice{" "}
+          <span className="text-[#1A73E8] text-[17px] ml-4">
+            Contact Expart
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
